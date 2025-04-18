b8d52a859ea5293caba20d8d2d9c0002d466e6da
Abderrahman SOUFIANE <Abderrahman.SOUFIANE@sanlam.ma>
2025-04-18 10:36:20 +0100

    update

--- SA.CheckTrackingPlatform.Domains.Management/Entities/Notification.cs
+++ SA.CheckTrackingPlatform.Domains.Management/Entities/Notification.cs
@@ -17,11 +17,6 @@ namespace SA.CheckTrackingPlatform.Domains.Management.Entities
         public virtual string Subject { get; set; }
         public virtual string Body { get; set; }
         public bool IsSeen { get; set; }
-<<<<<<< HEAD
-
-        [ForeignKey(nameof(InternalUser))] // propriéte de navigation 
-=======
->>>>>>> origin/develop
         public virtual int? InternalUserId { get; set; }
         public virtual short? NotificationTypeId { get; set; }
         public virtual int? InternalRoleId { get; set; }
