60372bf3cb9e5c360a4df5f3757684f95a1f432b
Abderrahman SOUFIANE <Abderrahman.SOUFIANE@sanlam.ma>
2025-04-21 10:53:59 +0100

    update

---
+++ SA.CheckTrackingPlatform.Infrastructures.Management/Repositories/Queries/ChecksQueryRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SA.CheckTrackingPlatform.Contexts.Management.Application;
+using SA.CheckTrackingPlatform.Domains.Management.Entities;
+using SA.CheckTrackingPlatform.Domains.Management.Repositories.Queries;
+using SA.CheckTrackingPlatform.Infrastructures.Management.Common;
+
+namespace SA.CheckTrackingPlatform.Infrastructures.Management.Repositories.Queries
+{
+    public class ChecksQueryRepository : QueryRepository<Checks>, IChecksQueryRepository
+    {
+        #region Fields 
+
+        protected readonly ApplicationContext applicationContext;
+
+        #endregion Fields 
+
+        #region Constructors 
+
+        public ChecksQueryRepository(ApplicationContext applicationContext) : base()
+        {
+            this.applicationContext = applicationContext;
+        }
+
+        #endregion Constructors
+
+        #region Methods 
+
+        public async Task<Checks> GetByIdAsync(int id)
+        {
+            Checks query = await this.applicationContext.Checkes
+                .AsNoTrackingWithIdentityResolution()
+                .SingleOrDefaultAsync(o => o.Id == id);
+
+            return query;
+        }
+
+        #endregion
+    }
+}
