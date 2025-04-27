5b892db365933a06b7d95f273cd48af6c402ad10
Souhail-Ismail-Ouldelhkim <souhailouldelhkim9@gmail.com>
2025-04-27 04:10:59 +0100

    Coonexion réussi via Oracle XE

---
+++ SA.CheckTrackingPlatform.Contexts.Management/Application/Migrations/20250427020249_MigrationDockerOracle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SA.CheckTrackingPlatform.Contexts.Management.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationDockerOracle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
