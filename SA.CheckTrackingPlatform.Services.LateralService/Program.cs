bf93e660cb30e70aed6f62512b6274d24040a7ac
Souhail-Ismail-Ouldelhkim <souhailouldelhkim9@gmail.com>
2025-04-29 17:04:43 +0100

    edit Program.cs for bank & Branchs & Status

--- SA.CheckTrackingPlatform.Services.LateralService/Program.cs
+++ SA.CheckTrackingPlatform.Services.LateralService/Program.cs
@@ -248,7 +248,6 @@ webApplicationBuilder.Services.AddTransient<IInternalRoleCommandRepository, Inte
 webApplicationBuilder.Services.AddTransient<IInternalUserCommandRepository, InternalUserCommandRepository>();
 webApplicationBuilder.Services.AddTransient<INotificationCommandRepository, NotificationCommandRepository>();
 webApplicationBuilder.Services.AddTransient<INotificationTypeCommandRepository, NotificationTypeCommandRepository>();
-
 webApplicationBuilder.Services.AddTransient<IInternalRoleQueryRepository, InternalRoleQueryRepository>();
 webApplicationBuilder.Services.AddTransient<IInternalUserQueryRepository, InternalUserQueryRepository>();
 webApplicationBuilder.Services.AddTransient<IChecksQueryRepository , ChecksQueryRepository>();
@@ -256,6 +255,8 @@ webApplicationBuilder.Services.AddTransient<ITimelinesQueryRepository, TimelineQ
 webApplicationBuilder.Services.AddTransient<IBanksQueryRepository, BanksQueryRepository>();
 webApplicationBuilder.Services.AddTransient<IBranchsQueryRepository, BranchsQueryRepository>();
 webApplicationBuilder.Services.AddTransient<IStatusQueryRepository, StatusQueryRepository>();
+
+
 webApplicationBuilder.Services.AddTransient<IInternalUserInternalRoleQueryRepository, InternalUserInternalRoleQueryRepository>();
 webApplicationBuilder.Services.AddTransient<INotificationQueryRepository, NotificationQueryRepository>();
 webApplicationBuilder.Services.AddTransient<INotificationTypeQueryRepository, NotificationTypeQueryRepository>();
