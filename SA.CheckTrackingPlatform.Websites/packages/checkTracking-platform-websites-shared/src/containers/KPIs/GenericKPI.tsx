c9bf6275453cf92beaac28a05a5644959e0c745b
Abderrahman SOUFIANE <Abderrahman.soufiane@sanlam.ma>
2025-05-08 17:30:30 +0100

    update

---
+++ SA.CheckTrackingPlatform.Websites/packages/checkTracking-platform-websites-shared/src/containers/KPIs/GenericKPI.tsx
@@ -0,0 +1,129 @@
+import { FormattedMessage } from "react-intl";
+import { GeneralHelper } from "@checkTracking/helpers";
+import {
+  Button,
+  CardContainer,
+  Grid,
+  Icons,
+  Tabs,
+  Theme,
+  Typography,
+} from "@checkTracking/ui-kit";
+import { CSSProperties } from "react";
+import { CircularProgress } from "@mui/material";
+import { useSelector } from "react-redux";
+
+interface contentProps {
+  count: number;
+  code: string;
+  description: string;
+  countColor?: CSSProperties["color"];
+  onClick: () => any;
+  isHideButton: boolean;
+}
+
+interface Props {
+  title: string;
+  contents: Array<contentProps>;
+}
+
+interface PropsKPI {
+  component: Props;
+  hasTeam?: boolean;
+  tabs?: {
+    label: string;
+  }[];
+  selectedTab?: number;
+  setSelectedTab?: any;
+}
+
+const KPICard = ({
+  component,
+  hasTeam,
+  tabs,
+  selectedTab,
+  setSelectedTab,
+}: PropsKPI) => {
+  const renderTabs = () =>
+    tabs &&
+    setSelectedTab && (
+      <Tabs
+        tabs={tabs}
+        value={selectedTab}
+        onChange={(event: any, newValue: number) =>
+          setSelectedTab && setSelectedTab(newValue)
+        }
+        {...(tabs.length > 5 && { variant: "scrollable" })}
+      />
+    );
+
+  // const {
+  //   responseData: exportFileExcelRenovel,
+  //   isLoading,
+  //   error,
+  // } = useSelector((state: any) => state.exportFileExcelRenovel);
+
+  const renderContent = (contents: any) => (
+    <Grid container direction="row" mt={2}>
+      {contents?.map((content: any, index: number) => (
+        <Grid key={content.description + index} item display="flex">
+          <Typography
+            variant="h4"
+            color={content.countColor ?? Theme.theme.palette.primary.main}
+            fontSize={40}
+          >
+            {GeneralHelper.formatNumber(content.count)}
+          </Typography>
+          {content.isHideButton ? (
+            <Button
+              variant="text"
+              endIcon={<Icons.Download />}
+              px={2}
+              onClick={() => content.onClick && content.onClick()}
+              // disabled={isLoading}
+            >
+              {/* {isLoading && (
+                <CircularProgress color="primary" size={24} sx={{ mr: 1 }} />
+              )} */}
+              {content.description}
+            </Button>
+          ) : null}
+        </Grid>
+      ))}
+    </Grid>
+  );
+
+  return (
+    <CardContainer minHeight={170}>
+      <Typography
+        variant="h5"
+        color={
+          // @ts-ignore
+          Theme.theme.palette.base.greyDark
+        }
+      >
+        <FormattedMessage id={component.title} />
+      </Typography>
+      {hasTeam && renderTabs()}
+      {renderContent(component.contents)}
+    </CardContainer>
+  );
+};
+
+export const GenericKPI = ({
+  component,
+  hasTeam,
+  tabs,
+  selectedTab,
+  setSelectedTab,
+}: PropsKPI) => {
+  return (
+    <KPICard
+      component={component}
+      hasTeam={hasTeam}
+      setSelectedTab={setSelectedTab}
+      selectedTab={selectedTab}
+      tabs={tabs}
+    />
+  );
+};
---
+++ SA.CheckTrackingPlatform.Websites/packages/checkTracking-platform-websites-shared/src/containers/KPIs/KPIs.tsx
@@ -0,0 +1,87 @@
+import {
+  Alert,
+  CardContainer,
+  Grid,
+  Theme,
+  Typography,
+} from "@checkTracking/ui-kit";
+import { GenericKPI } from "./GenericKPI";
+
+export const KPIs = () => {
+  return (
+    <Grid
+      container
+      sx={{ pt: 7, px: 8, pr: 6 }}
+      backgroundColor={
+        // @ts-ignore
+        Theme.theme.palette.base.main
+      }
+    >
+      {
+        <Grid container direction="row" spacing={2} xl={12}>
+          <Grid item xs={3}>
+            <GenericKPI
+              component={{
+                title: "Nb cheque ",
+                contents: [
+                  {
+                    code: "1",
+                    count: 0, //GetCountReinsurances?.count ?? 0,
+                    countColor: "red",
+                    description: "Télécharger",
+                    onClick() {
+                      //   KPIsService.ExportFileExcelRenovel &&
+                      //     KPIsService.ExportFileExcelRenovel();
+                      <Alert icon="info" />;
+                    },
+                    isHideButton: true,
+                  },
+                ],
+              }}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <GenericKPI
+              component={{
+                title: "Nb cheque ",
+                contents: [
+                  {
+                    code: "1",
+                    count: 0, //GetCountReinsurances?.count ?? 0,
+                    countColor: "red",
+                    description: "Télécharger",
+                    onClick() {
+                      //   KPIsService.ExportFileExcelRenovel &&
+                      //     KPIsService.ExportFileExcelRenovel();
+                    },
+                    isHideButton: true,
+                  },
+                ],
+              }}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <GenericKPI
+              component={{
+                title: "Nb cheque ",
+                contents: [
+                  {
+                    code: "1",
+                    count: 0, //GetCountReinsurances?.count ?? 0,
+                    countColor: "red",
+                    description: "Télécharger",
+                    onClick() {
+                      //   KPIsService.ExportFileExcelRenovel &&
+                      //     KPIsService.ExportFileExcelRenovel();
+                    },
+                    isHideButton: true,
+                  },
+                ],
+              }}
+            />
+          </Grid>
+        </Grid>
+      }
+    </Grid>
+  );
+};
