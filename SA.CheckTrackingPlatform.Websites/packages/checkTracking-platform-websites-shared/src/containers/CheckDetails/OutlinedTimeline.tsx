ec34048fca7dc85e32cd5a8de95facda1cc4a8a1
Abderrahman SOUFIANE <Abderrahman.soufiane@sanlam.ma>
2025-05-19 11:18:16 +0100

    update

--- SA.CheckTrackingPlatform.Websites/packages/checkTracking-platform-websites-shared/src/containers/CheckDetails/OutlinedTimeline.tsx
+++ SA.CheckTrackingPlatform.Websites/packages/checkTracking-platform-websites-shared/src/containers/CheckDetails/OutlinedTimeline.tsx
@@ -7,100 +7,29 @@ import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
 import { Typography } from "@mui/material";
 
-interface OutlinedTimelineProps {}
-
-export default function OutlinedTimeline() {
+export default function OutlinedTimeline({ Checks }: { Checks: [] }) {
   return (
-    <Timeline position="alternate">
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="success" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography variant="h6">Reception BO</Typography>
-          <Typography variant="body2" color="text.secondary">
-            date et le check numéro de la banque
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="success" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography variant="h6">Reception BO</Typography>
-          <Typography variant="body2" color="text.secondary">
-            date et le check numéro de la banque
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="secondary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography variant="h6">Reception Métier</Typography>
-          <Typography variant="body2" color="text.secondary">
-            Status : Date de passage : Motif : Commentaire :
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="secondary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography variant="h6">Reception Métier</Typography>
-          <Typography variant="body2" color="text.secondary">
-            Date , le check numéro avec le status
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography variant="h6">Envoie BO</Typography>
-          <Typography variant="body2" color="text.secondary">
-            Date Validation du chèques
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="secondary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography variant="h6">Reception Métier</Typography>
-          <Typography variant="body2" color="text.secondary">
-            Date , le check numéro avec le status
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary" />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography variant="h6">Envoie Métier</Typography>
-          <Typography variant="body2" color="text.secondary">
-            date , chéque n° prés à l'envoie au métier
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-    </Timeline>
+    <>
+      {Checks.map((check: any) => {
+        return (
+          <>
+            <Timeline position="alternate">
+              <TimelineItem>
+                <TimelineSeparator>
+                  <TimelineDot variant="outlined" color="success" />
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Typography variant="h6">{check.yuerw}</Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {check.test}
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+            </Timeline>
+          </>
+        );
+      })}
+    </>
   );
 }
