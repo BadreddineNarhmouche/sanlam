d9e1104656d50b7267d909aa9fc85fa270450ced
Abderrahman SOUFIANE <Abderrahman.soufiane@sanlam.ma>
2025-06-23 10:09:15 +0100

    update

--- SA.CheckTrackingPlatform.ServiceEngines.Management/Checkes/Responses/GetChecksByIdResponse.cs
+++ SA.CheckTrackingPlatform.ServiceEngines.Management/Checkes/Responses/GetChecksByIdResponse.cs
@@ -11,7 +11,11 @@ namespace SA.CheckTrackingPlatform.ServiceEngines.Management.Checkes.Responses
         public int BankId { get; set; }
         public int BranchId { get; set; }
         public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public string BankName { get; set; }
+        public string BranchName { get; set; }
         public DateTime CreationDate { get; set; }
+        public int UserId { get; set; }
         public string CheckNumber { get; set; }
         public string LotNumber { get; set; }
         public string RecipientName { get; set; }
@@ -19,10 +23,50 @@ namespace SA.CheckTrackingPlatform.ServiceEngines.Management.Checkes.Responses
         public string AccountNumber { get; set; }
         public string RegisterOrderNumber { get; set; }
         public decimal TransactionNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string BeneficiaryName { get; set; }
+        public StatusItems StatusItems { get; set; }
+        public InternalUser InternalUser { get; set; }
+        public ReasonMoveItems ReasonMoveItems { get; set; }
 
-        public ICollection<Timeline> Timelines { get; set; }
+        public ICollection<TimelineItems> Timelines { get; set; }
 
-        #endregion
+        #endregion 
+    }
+    public class TimelineItems
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+        public StatusItems StatusItems { get; set; }
+
+        public InternalUserItems InternalUserItem { get; set; }
+
+        public ReasonMoveItems ReasonMoveItems { get; set; }
+    }
+
+    public class StatusItems
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Label { get; set; }
+    }
+
+
+    public class InternalUserItems
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class ReasonMoveItems
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Label { get; set; }
     }
 }
--- SA.CheckTrackingPlatform.Websites/apps/checkTracking-platform-websites-backoffice/src/pages/AppLayout/index.tsx
+++ SA.CheckTrackingPlatform.Websites/apps/checkTracking-platform-websites-backoffice/src/pages/AppLayout/index.tsx
@@ -1,84 +1,59 @@
 import { INotificationService } from "@checkTracking/helpers";
 
 import {
-  NavigationBar,
-  PermissionsGate,
-  // ROLE,
-  SCOPE,
+    NavigationBar,
+    PermissionsGate,
+    SCOPE,
 } from "@checkTracking/shared";
 
 import { Grid } from "@checkTracking/ui-kit";
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
 import { injectIntl, useIntl } from "react-intl";
 import { Outlet, useLocation } from "react-router-dom";
 import { PAGES } from "../../config/navigation";
 
 const AppLayout = () => {
-  const intl = useIntl();
-  // const dispatch = useDispatch();
-  const navItems = {
-    applicationName: intl.formatMessage({ id: "applicationName" }),
-    links: [
-      {
-        label: intl.formatMessage({ id: "nav_bar.home_tab" }),
-        to: PAGES.HOME,
-        // roles: [
-        //   ROLE.SUBSCRIPTIONS_EXECUTOR,
-        //   ROLE.SUBSCRIPTIONS_MANAGER,
-        //   ROLE.RECOVERIES_EXECUTOR,
-        //   ROLE.RECOVERIES_MANAGER,
-        //   ROLE.checkTrackingS_EXECUTOR,
-        //   ROLE.checkTrackingS_MANAGER,
-        //   ROLE.ACCOUNTING_EXECUTOR,
-        //   ROLE.ACCOUNTING_MANAGER,
-        // ],
-      },
-       {
-        label: intl.formatMessage({ id: "nav_bar.treatment_tab" }),
-        to: PAGES.TREATMENT_CHECK,
-        // roles: [
-        //   ROLE.SUBSCRIPTIONS_EXECUTOR,
-        //   ROLE.SUBSCRIPTIONS_MANAGER,
-        //   ROLE.RECOVERIES_EXECUTOR,
-        //   ROLE.RECOVERIES_MANAGER,
-        //   ROLE.checkTrackingS_EXECUTOR,
-        //   ROLE.checkTrackingS_MANAGER,
-        //   ROLE.ACCOUNTING_EXECUTOR,
-        //   ROLE.ACCOUNTING_MANAGER,
-        // ],
-      },
-      {
-        label: intl.formatMessage({ id: "nav_bar.help_tab" }),
-        to: PAGES.HELP,
-      },
-    ],
-  };
+    const intl = useIntl();
+    const navItems = {
+        applicationName: intl.formatMessage({ id: "applicationName" }),
+        links: [
+            {
+                label: intl.formatMessage({ id: "nav_bar.home_tab" }),
+                to: PAGES.HOME,
+            },
+            {
+                label: intl.formatMessage({ id: "nav_bar.treatment_tab" }),
+                to: PAGES.TREATMENT_CHECK,
+            },
+            {
+                label: intl.formatMessage({ id: "nav_bar.help_tab" }),
+                to: PAGES.HELP,
+            },
+        ],
+    };
 
-  const showNavigationBar = ![
-    PAGES.NOT_FOUND,
-    PAGES.DETAILS_CHECK,
-  ].includes(useLocation().pathname);
 
-  const services: INotificationService = {};
+    const showNavigationBar = ![
+        PAGES.NOT_FOUND,
+        PAGES.DETAILS_CHECK,
+    ].includes(useLocation().pathname);
 
-  // const location = useLocation();
+    const services: INotificationService = {};
 
-  return (
-    <>
-      <PermissionsGate scopes={[SCOPE.CAN_VIEW_MENU]} delegatedNoAccessProps>
-        <NavigationBar
-          navItems={navItems}
-          services={services}
-          showNavigationBar={showNavigationBar}
-        />
-      </PermissionsGate>
+    return (
+        <>
+            <PermissionsGate scopes={[SCOPE.CAN_VIEW_MENU]} delegatedNoAccessProps>
+                <NavigationBar
+                    navItems={navItems}
+                    services={services}
+                    showNavigationBar={showNavigationBar}
+                />
+            </PermissionsGate>
 
-      <Grid mt={5}>
-        <Outlet />
-      </Grid>
-    </>
-  );
+            <Grid mt={5}>
+                <Outlet />
+            </Grid>
+        </>
+    );
 };
 
 export default injectIntl(AppLayout);
