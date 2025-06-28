e1a72eab08a01fbf4bfbaf8a1586e7b347c4576f
Souhail-Ismail-Ouldelhkim <souhailouldelhkim9@gmail.com>
2025-06-28 19:29:13 +0100

    Quantum

--- SA.CheckTrackingPlatform.Websites/packages/checkTracking-platform-websites-helpers/lib/api/types/service.d.ts
+++ SA.CheckTrackingPlatform.Websites/packages/checkTracking-platform-websites-helpers/lib/api/types/service.d.ts
@@ -1,34 +1,25 @@
-import {
-  FilterByAllChecks,
-  FilterCriteriaChecks,
-  type FilterCriteriaNotifications,
-  type Notification,
-} from "./domain";
+import { FilterByAllChecks, FilterCriteriaChecks, type FilterCriteriaNotifications, type Notification } from "./domain";
 export interface INotificationService {
-  getAllNotificationsByCriteria?: (
-    criteria: FilterCriteriaNotifications
-  ) => void;
-  countAllNotificationsByCriteria?: (
-    criteria: FilterCriteriaNotifications
-  ) => void;
-  updateNotification?: (notification: Notification) => void;
+    getAllNotificationsByCriteria?: (criteria: FilterCriteriaNotifications) => void;
+    countAllNotificationsByCriteria?: (criteria: FilterCriteriaNotifications) => void;
+    updateNotification?: (notification: Notification) => void;
 }
 export interface IChecksService {
-  getAllChecksByCriteria?: (criteria: FilterCriteriaChecks) => void;
-  getAllChecks?: (criteria: FilterByAllChecks) => void;
+    getAllChecksByCriteria?: (criteria: FilterCriteriaChecks) => void;
+    getAllChecks?: (criteria: FilterByAllChecks) => void;
 }
 export interface IDetailsChecksService {
-  getCheckById?: (Id: string) => void;
+    getCheckById?: (Id: string) => void;
 }
 export interface IStatusService {
-  getAllStatus?: () => void;
+    getAllStatus?: () => void;
 }
 export interface IReasonMoveService {
-  AllReasonMoves: (To: any) => void;
+    AllReasonMoves: (To: any) => void;
 }
 export interface ITimeLineService {
-  CreateTimeLine: (payload: any) => void;
+    CreateTimeLine: (payload: any) => void;
 }
 export interface IKPIService {
-  GetKPIs: () => void;
+    GetKPIs: () => void;
 }
